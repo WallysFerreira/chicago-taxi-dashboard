12988f19254799eb46660f222263629decd5948b
Wallys Ferreira <wallysfsilva@gmail.com>
2024-06-10 20:10:32 -0300

    show amount of trips

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ import pandas as pd
 
 data = pd.read_csv("taxi-trips.csv")
 
-st.write(data.groupby(["Company"]).size())
+selected_company = st.selectbox(label="Company", options=data["Company"].unique(), index=None)
+
+if selected_company != None:
+    st.metric(label="Trips", value=data.groupby(["Company"]).size()[selected_company])
+else:
+    st.metric(label="Trips", value=data.groupby(["Company"]).size().sum())
